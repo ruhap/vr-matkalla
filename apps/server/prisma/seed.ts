32e8522314c4c60edbe88bd552ce3fde73d1a964
rocawear <anttivep@gmail.com>
2023-06-17 16:50:29 +0300

    added prisma

---
+++ apps/server/prisma/seed.ts
@@ -0,0 +1,15 @@
+import { db } from "../src/db";
+
+async function main() {
+  console.log("seed started");
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
